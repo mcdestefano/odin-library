822dff756e5a520d11ab957da3c971ba8e77b689
mcdestefano <124924561+mcdestefano@users.noreply.github.com>
2023-03-29 14:23:31 -0400

    Clean up form for adding new books and add unimplemented button for removing books

--- script.js
+++ script.js
@@ -3,9 +3,31 @@ const myLibrary = [];
 const library = document.querySelector('.library');
 const newButton = document.querySelector('.new');
 const form = document.querySelector('.book-form');
+const submitButton = document.querySelector('.submit');
 
 newButton.addEventListener('click', () => {
   form.style.visibility = 'visible';
+});
+
+submitButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  const title = document.querySelector('#title');
+  const author = document.querySelector('#author');
+  const pages = document.querySelector('#pages');
+  const read = document.querySelector('#read');
+  const newBook = new Book(
+    title.value,
+    author.value,
+    pages.value,
+    read.checked
+  );
+  addBookToLibrary(newBook);
+  displayBooks();
+  form.style.visibility = 'hidden';
+  title.value = '';
+  author.value = '';
+  pages.value = '';
+  read.checked = false;
 });
 
 function Book(title, author, pages, read) {
@@ -26,12 +48,31 @@ function addBookToLibrary(book) {
 }
 
 function displayBooks() {
+  library.textContent = null;
   myLibrary.forEach((book) => {
-    const eBook = document.createElement('div');
-    eBook.classList.add('book');
+    const spot = document.createElement('div');
+    const removeButton = document.createElement('button');
+    removeButton.classList.add('remove');
+    removeButton.textContent = 'Remove book from library';
+    removeButton.addEventListener('click', () => {
+      // should remove corresponding spot from DOM and book from library
+    });
+    const eBook = document.createElement('p');
+    spot.classList.add('book');
+    const num = myLibrary.indexOf(book);
+    const label = 'book' + num;
+    eBook.classList.add(label);
     eBook.textContent = book.info();
-    library.appendChild(eBook);
+    spot.appendChild(eBook);
+    spot.appendChild(removeButton);
+    library.appendChild(spot);
   });
+}
+
+function removeBook(book) {
+  const index = myLibrary.indexOf(book);
+  myLibrary.splice(index, 1);
+  displayBooks();
 }
 
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
