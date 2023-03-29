f40390e8cae1573e7852c2f58bb2f07a9b3b05aa
mcdestefano <124924561+mcdestefano@users.noreply.github.com>
2023-03-29 13:53:51 -0400

    Add library display and simple form for adding new book

--- script.js
+++ script.js
@@ -1,16 +1,57 @@
+const myLibrary = [];
+
+const library = document.querySelector('.library');
+const newButton = document.querySelector('.new');
+const form = document.querySelector('.book-form');
+
+newButton.addEventListener('click', () => {
+  form.style.visibility = 'visible';
+});
+
 function Book(title, author, pages, read) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
   this.info = function () {
     if (read) {
-      return `${title} by ${author}, ${pages} pages, read.`
+      return `${title} by ${author}, ${pages} pages, read.`;
     }
-    return `${title} by ${author}, ${pages} pages, not read yet.`
-  }
+    return `${title} by ${author}, ${pages} pages, not read yet.`;
+  };
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false)
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
 
-console.log(theHobbit.info())
+function displayBooks() {
+  myLibrary.forEach((book) => {
+    const eBook = document.createElement('div');
+    eBook.classList.add('book');
+    eBook.textContent = book.info();
+    library.appendChild(eBook);
+  });
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
+const theGreatGatsby = new Book(
+  'The Great Gatsby',
+  'F. Scott Fitzgerald',
+  208,
+  false
+);
+const oneHundredYearsOfSolitude = new Book(
+  'One Hundred Years Of Solitude',
+  'Gabriel Garcia Marquez',
+  448,
+  false
+);
+
+addBookToLibrary(theHobbit);
+addBookToLibrary(theGreatGatsby);
+addBookToLibrary(oneHundredYearsOfSolitude);
+
+console.log(theHobbit.info());
+
+displayBooks();
